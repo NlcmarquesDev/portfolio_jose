a1626dd51e8a533ec1bc5a1deea29567e22e8342
NlcmarquesDev <nunolcmarques@gmail.com>
2024-07-04 14:57:04 +0200

    fix bug with qury injection language

--- lang.php
+++ lang.php
@@ -2,20 +2,42 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+// if (isset($_GET['lang'])) {
+//     if ($_GET['lang'] != 'en' || $_GET['lang'] != 'pt' || $_GET['lang'] != 'be') {
+//         header('location:index.php');
+//         exit();
+//     }
+// }
 
 require get_language_file();
 function get_language_file()
 {
 
-    $_SESSION['lang'] = $_SESSION['lang'] ?? 'en';
-    if ($_GET['lang'] == 'en' || $_GET['lang'] == 'pt' || $_GET['lang'] == 'be') {
 
-        $_SESSION['lang'] = $_GET['lang'] ?? $_SESSION['lang'];
-        return ('languages/' . $_SESSION['lang'] . '.php');
-    } else {
-        header('location:index.php?lang=en');
-        exit();
+    $_SESSION['lang'] = $_SESSION['lang'] ?? 'en';
+    // if ($_GET['lang'] == 'en' || $_GET['lang'] == 'pt' || $_GET['lang'] == 'be' || $_GET['id']) {
+
+    if (isset($_GET['lang'])) {
+
+        if ($_GET['lang'] != 'en' && $_GET['lang'] != 'pt' && $_GET['lang'] != 'be') {
+            var_dump("ESTA NAO PERTENCE - " . $_GET['lang']);
+            header('location:index.php');
+            exit();
+        }
     }
+
+
+    $_SESSION['lang'] = $_GET['lang'] ?? $_SESSION['lang'];
+    // }
+    return ('languages/' . $_SESSION['lang'] . '.php');
+
+
+    // }
+    // else {
+    //     header('location:index.php');
+    //     exit();
+    // }
+
 }
 function __($str)
 {
